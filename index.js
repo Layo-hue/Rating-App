bc4166bd62d68eddabb0b3641f4b6c47a1707cc9
LayoLayo <mariafadeyi@gmail.com>
2023-03-17 21:52:09 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let container = document.getElementById("container");
+let containerOne = document.getElementById("container1");
+let one = document.getElementById("one");
+let two = document.getElementById("two");
+let three = document.getElementById("three");
+let four = document.getElementById("four");
+let five = document.getElementById("five");
+
+function displayThanks() {
+  containerOne.classList.add("container-Visible");
+  // container.classList.remove("container-Hidden");
+}
